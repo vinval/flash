6dc63501cbeabf37cc2ed404cb83f63967d146d7
Vincenzo Valrosso <vincenzo.valrosso@gmail.com>
2019-01-18 18:53:02 +0100

    Update site.js

--- v1.0.0/site.js
+++ v1.0.0/site.js
@@ -1,54 +1,402 @@
-const styles = {
-    main: {
-        width: SCREEN.width,
-        height: SCREEN.height,
-        display: "flex",
-        justifyContent: "center",
-        alignItems: 'center',
-        backgroundColor: '#333',
-        color: "white"
-    },
-    child: {
-        width: SCREEN.height/3,
-        height: SCREEN.height/3,
-        backgroundColor: '#fff',
-        borderRadius: {
-            range: [0,SCREEN.height/1.5],
-            duration: 300,
-        },
-        display: "flex",
-        justifyContent: "center",
-        alignItems: 'center',
-        color: '#222',
-        opacity: {
-            range: [0.0,0.2,1.0],
-            duration: 300
+window.jdomActiveElement = null;
+
+const SCREEN = {
+    width:  window.innerWidth
+        || document.documentElement.clientWidth
+        || document.body.clientWidth,
+    height: window.innerHeight
+        || document.documentElement.clientHeight
+        || document.body.clientHeight
+}
+
+function JDom (dom, doc) {
+    doc = doc 
+        ? typeof doc === "string"
+            ? document.getElementById(doc)
+            : doc 
+        : document.body;
+    doc.innerHTML = ""; 
+    domBuilder(dom, doc);
+    window.jdomGlobal = dom;
+    domElementsObserve();
+    //domObjectObserve();
+    if (jdomActiveElement) {
+        const ae = document.getElementById(jdomActiveElement);
+        ae.focus();
+        ae.value = ae.value
+    }
+    
+    function domObjectObserve() {
+        document.addEventListener("change", function(){
+            JDom(jdomGlobal);
+        })
+    }
+    
+    function domElementsObserve() {
+        Array.prototype.slice.call(document.querySelectorAll("input")).map(function(input){
+            input.addEventListener('change', function (evt) {
+                if (this.type !== "text") {
+                    if (this.type === "radio") {
+                        const radios = this.parentNode.querySelectorAll('input[type="radio"]');
+                        Array.prototype.slice.call(radios).map(function(radio){
+                            domVariableSearch(radio.id, "__delete__", "checked");        
+                        })
+                    }
+                    domVariableSearch(this.id, this.checked ? true : "__delete__", "checked");
+                }
+            });
+            input.addEventListener('input', function (evt) {
+                domVariableSearch(this.id, this.value, "value");
+            });
+        })
+    }
+    
+    function domVariableSearch(id, value, property, obj) {
+        jdomActiveElement = id;
+        obj = obj ? obj : window.jdomGlobal;
+        try {
+            obj.map(function(o){
+                if (o.id === id) {
+                    o[property] = value;
+                    if (value === "__delete__") delete o[property]
+                    return false;
+                }
+                if (o.childs) domVariableSearch(id, value, property, o.childs)
+            })
+        } catch (e) {
+            console.log(e)
         }
+    }
+
+    function cssPseudo (domElement, elem, pseudo) {
+        var style = {};
+        var eventIn, eventOut;
+        switch (pseudo) {
+            case "hover": eventIn = "mouseenter"; eventOut = "mouseleave"; break;
+            case "focus": eventIn = "focus"; eventOut = "blur"; break;
+            case "active": eventIn = "mousedown"; eventOut = "mouseup"; break;
+        };
+        domElement.addEventListener(eventIn, function(){
+            style = __f(elem.id).self.style;
+            style = style ?  deformattingStyle(style) : {};
+            __f(elem.id).self.element.style = formattingStyle(
+                __c(style, elem[pseudo]),
+                elem,
+                domElement
+            )
+        })
+        domElement.addEventListener(eventOut, function(){
+            __f(elem.id).self.element.style = formattingStyle(
+                style, 
+                elem,
+                domElement
+            )
+        })
+    }
+
+    function domBuilder (domObject, positionInDom) {
+        try {
+            domObject.map(function(elem){
+                if (necessaryTagsCheck(elem)) {
+                    var domElement = document.createElement(elem.tag || "div");
+                    elem.id = elem.id ? elem.id : randomID();
+                    Object.keys(elem).map(function(item){
+                        try {
+                            if (excludeTagsFromBuilding(item)) {
+                                switch (item) {
+                                    case "style": elem[item] = formattingStyle(elem[item], elem, domElement); break;
+                                }
+                                if (elem[item]) domElement.setAttribute(item, domEvaluateString(elem[item], elem));
+                            } else {
+                                switch (item) {
+                                    case "html": domElement.innerHTML = domEvaluateString(elem[item], elem); break;
+                                    case "hover": cssPseudo (domElement, elem, "hover"); break;
+                                    case "focus": cssPseudo (domElement, elem, "focus"); break;
+                                    case "active": cssPseudo (domElement, elem, "active"); break;
+                                }
+                            }
+                        } catch (e) {}
+                    })
+                    positionInDom.appendChild(domElement);
+                    if (elem["childs"]) domBuilder(elem["childs"], domElement);
+                }
+                elem.element = domElement;
+            })
+        } catch (e) {
+            if (dom.length) {
+                console.error("JDom::", e)
+                console.info("JDom:: childs attribute must be array")
+            }
+        }
+        
+        function randomID() {
+            const cases = "@-0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            var id = "";
+            for (var n=0; n<10; n++) id+=cases[Math.floor(Math.random()*cases.length)];
+            return id;
+        }
+    }
+    
+    function domEvaluateString(str, domObject) {
+        const regExp = /{{(.*?)}}/g;
+        var matches = regExp.exec(str);
+        if (matches && str.indexOf("SELF")!==-1) {
+            str = str.replace(/SELF/g, "__f('"+domObject.id+"').self").replace(/{{/g,"").replace(/}}/g,"");
+            matches = regExp.exec(str);
+        }
+        try {
+            const result = eval(matches[1]);
+            return result;
+        } catch (e) {
+            return str;
+        }
+    }
+        
+    function deformattingStyle (style) {
+        var s = style.split(";");
+        var r = {};
+        s.map(function(e){
+            const prop = e
+                .split(":")[0]
+                .split("-")
+                .map(function(a,k){
+                    if (k>0) 
+                        return a.charAt(0).toUpperCase()+a.substr(1)
+                    else
+                        return a
+                })
+                .join("");
+            const value = e
+                .split(":")[1]
+            r[prop] = value
+        })
+        return r;
+    }
+    
+    function formattingStyle(style, domObject, domElement) {
+        if (typeof style === "string") return domEvaluateString(style, domObject);
+        try {
+            var styleArray = [];
+            Object.keys(style).map(function(item){
+                const match = item.match(/([A-Z])/g);
+                var itemTmp = item;
+                if (match) match.map(function(m){
+                    item = item.replace(m,"-"+m.toLowerCase());
+                })
+                if (typeof style[itemTmp] === "number" && itemTmp !== "flex") {
+                    style[itemTmp] = style[itemTmp]+"px";
+                }
+                if (typeof style[itemTmp] === "string") 
+                    style[itemTmp] = typeof domEvaluateString(style[itemTmp], domObject) === 'number' ?
+                        domEvaluateString(style[itemTmp], domObject)+"px"
+                        : domEvaluateString(style[itemTmp], domObject);
+                if (typeof style[itemTmp] === "object") {
+                    const props = style[itemTmp];
+                    const delay = props.delay || 0;
+                    const duration = props.duration || 1000;
+                    const rangeLength = props.range.length-1;
+                    const timeout = (duration/rangeLength)/10;
+                    const type = props.type || (itemTmp !== "opacity") ? "px" : "";
+                    var loops = 0;
+                    looping()
+                    function looping() {
+                        var c = 0;
+                        try {
+                            var startValue = domEvaluateString(props.range[c], domObject);
+                            var stopValue = domEvaluateString(props.range[c+1], domObject);
+                            var delta = (stopValue-startValue)>1 ? 1 : -1;
+                            var intervalCounter = 0;
+                            var increment = Number((Math.abs(stopValue-startValue)/(timeout)).toFixed(2));
+                            domElement.style[item] = Number(startValue.toFixed(2))+type;
+                            setTimeout(function(){
+                                var mainInterval = setInterval(function(){
+                                    domElement.style[item] = Number(startValue.toFixed(2))+type;
+                                    startValue+=(increment*(itemTmp !== "opacity" ? delta : 1));
+                                    intervalCounter++
+                                    if (intervalCounter>=timeout) {
+                                        intervalCounter=0;
+                                        c++;
+                                        if (c>=rangeLength) {
+                                            clearInterval(mainInterval)
+                                            domElement.style[item] = Number(stopValue.toFixed(2))+type;
+                                            loops++;
+                                            if (typeof props.loop === "boolean" && props.loop) looping();
+                                            if (typeof props.loop === "number" && props.loop>loops) looping();
+                                        };
+                                        startValue = domEvaluateString(props.range[c], domObject);
+                                        stopValue = domEvaluateString(props.range[c+1], domObject);
+                                        delta = (stopValue-startValue)>1 ? 1 : -1;
+                                        increment = Number((Math.abs(stopValue-startValue)/timeout).toFixed(2));
+                                    }
+                                },1)
+                            },delay)
+                        } catch (e) {
+                            console.error("JDom::", "please check your range. It must have at least 2 elements.");
+                        }
+                    }
+                }
+                styleArray.push(item+":"+style[itemTmp]);
+            })
+            return styleArray.join(";")
+        } catch (e) {}
+        return null;
+    }
+
+    function excludeTagsFromBuilding(item) {
+        const attrList = [
+            "tag",
+            "childs",
+            "html",
+            "element",
+            "hover",
+            "focus",
+            "active",
+            "visited"
+        ];
+        if (attrList.indexOf(item)!==-1) 
+            return false;
+        return true
+    }
+
+    function necessaryTagsCheck (element) {
+        const refs = Object.keys(element);
+        var tagsList = [];
+        var check = true;
+        tagsList.map(function(tag){
+            if (refs.indexOf(tag)===-1) {
+                check = false;
+                console.error("JDom::", tag+" element is necessary");
+            }
+        })
+        if (!check) {
+            console.info("JDom:: info");
+            console.info("Necessary Tags: "+tagsList);
+        }
+        return check;
     }
 }
 
-JDom([
-    {
-        id: "main",
-        style: styles.main,
-        childs: [
-            {
-                id: "child",
-                html: "Hello JDom!",
-                style: styles.child,
-                hover: {
-                    color: "red"
+const __c = JDomConcat = function () {
+    const len = arguments.length;
+    var ret = {};
+    for (var i=0; i<len; i++) {
+      for (p in arguments[i]) {
+        try {
+            if (arguments[i].hasOwnProperty(p)) {
+              ret[p] = arguments[i][p];
+            }
+        } catch (e) {
+            console.error("JDom::", "check your arguments in JDomConcat. They must be objects.");
+        }
+      }
+    }
+    return ret;
+}
+
+const __p = JDomPrettify = function () {
+    document.body.style.boxSizing = "border-box";
+    document.body.style.padding = "0px";
+    document.body.style.margin = "0px";
+}
+
+const __t = JDomTransform = function (htmlQueryReference, movementsObject, duration, callback) {
+    const elements = document.querySelectorAll(htmlQueryReference);
+    Array.prototype.slice.call(elements).map(function(elem) {
+        const i = setInterval(frame, 1);
+        var incrementation = 0;
+        elem.style.transform = settingMovement (incrementation);
+        function frame() {
+            if (incrementation == 1) {
+                clearInterval(i);
+            } else {
+                incrementation+= duration ? (6/duration) : 0.1;
+                if (incrementation > 1) {
+                    incrementation = 1;
+                    if (callback) setTimeout(function(){callback()},50);
                 }
             }
-        ]
+            elem.style.transform = settingMovement (incrementation);
+        }
+    })
+    
+    function settingMovement (inc) {
+        var transformation = [];
+        Object.keys(movementsObject).map(function(mov){
+            var type = "";
+            if (mov.indexOf("translate")!==-1) type = "px";
+            if (mov.indexOf("rotate")!==-1) type = "deg";
+            transformation.push(
+                mov+"("+(movementsObject[mov][0]+((movementsObject[mov][1]-movementsObject[mov][0])*inc))+type+")"
+            );
+        })
+        return transformation.join(" ");
     }
-]);
+}
 
-JDomTransform(
-    "#child",
-    {
-        scale: [10,1]
-    },
-    1500
-)
-JDomPrettify();
+const __f = JDomFind = function (id) {
+    var found = false
+    function recursive(id, obj, index, parent) {
+        const jg = obj ? obj : window.jdomGlobal;
+        var index = index ? index : [];
+            parent = parent ? parent : null;
+        if (typeof jg === "object" && jg.length > 0) {
+            jg.map(function(e,k){
+                if (e.id === id) {
+                    index.push(k);
+                    found = {
+                        self: e,
+                        path: index,
+                        parent: parent
+                    };
+                    return true;
+                } else if (e.childs) {
+                    index.push(k);
+                    recursive(id, e.childs, index, e)
+                } else {
+                    return false
+                }
+            })
+        }
+    }
+    recursive(id);
+    return found
+}
+
+const __m = JDomModule = function(filePath) {
+    if (window.location.protocol !== "file:") {
+        const splice = filePath.split(".");
+        const extension = splice.length>1 ? "" : ".json";
+        // Load json file;
+        function JDomloadTextFileAjaxSync(filePath, mimeType) {
+            var xmlhttp=new XMLHttpRequest();
+            xmlhttp.open("GET",filePath+extension,false);
+            if (mimeType != null) {
+                if (xmlhttp.overrideMimeType) {
+                    xmlhttp.overrideMimeType(mimeType);
+                }
+            }
+            xmlhttp.send();
+            if (xmlhttp.status==200) {
+                return xmlhttp.responseText;
+            }
+            else {
+                // TODO Throw exception
+                return null;
+            }
+        }
+        var json = JDomloadTextFileAjaxSync(filePath, "application/json");
+        // Parse json
+        return JSON.parse(json);
+    } else {
+        console.error("Jdom::", "you cannot use JDomModule outside server");
+        return false;
+    } 
+};
+
+JDom.prototype.then = function(callback) {
+    return callback(jdomGlobal)
+}
+
+JDom.prototype.prettify = function () {
+    JDomPrettify()
+}
